5b33d5cf47c49cc577614a454bffbb30e4f9e97f
Aku Rouhe <aku.rouhe@aalto.fi>
2017-12-01 11:23:28 +0200

    Separate font installation

--- install-all.sh
+++ install-all.sh
@@ -55,13 +55,4 @@ else
 fi
 echo
 
-powerlinedir="$dotfilespath/powerlinefonts"
-if [ -e "$powerlinedir" ]; then
-  echo "Looks like powerlinefonts have already been downloaded in $powerlinedir"
-else
-  echo "Installing Powerline-patched Meslo with:"
-  git clone https://github.com/powerline/fonts "$powerlinedir"
-  "$powerlinedir"/install.sh 'Meslo Slashed'
-  echo "You have to set Meslo LG S for Powerline Bold 10pt as the font of your terminal emulator."
-fi
 
